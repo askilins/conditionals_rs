04ce1be456f06e2f0aa4263922ec04abdfda2c13
Aigars Šķiliņš <null>
2025-02-04 21:41:29 +0200

    Not Either Both

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+fn not (expr: bool) -> bool {
+    !expr
+}
+
+
+fn either (left: bool, right: bool) -> bool {
+    left || right
+}
+
+
+fn both (left: bool, right: bool) -> bool {
+    left && right
+}
