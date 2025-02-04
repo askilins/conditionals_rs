4ff306a42a5457c6ec06eeae2f49c39262408d61
Aigars Šķiliņš <93160726+askilins@users.noreply.github.com>
2025-02-04 23:59:50 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,17 @@
 // Returns true, if not false 
-#[inline(always)]
+#[inline]
 pub fn not (expr: bool) -> bool {
     !expr
 }
 
 // Returns true, if either left or right is true
-#[inline(always)]
+#[inline]
 pub fn either (left: bool, right: bool) -> bool {
     left || right
 }
 
 // Returns true, if both left and right are true
-#[inline(always)]
+#[inline]
 pub fn both (left: bool, right: bool) -> bool {
     left && right
 }
