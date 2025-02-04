9c66d4de1519518be237a92c7401c0d5d7780aca
Aigars Šķiliņš <null>
2025-02-04 23:15:08 +0200

    Added comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
+// Returns true, if not false 
 pub fn not (expr: bool) -> bool {
     !expr
 }
 
+// Returns true, if either left or right is true
 pub fn either (left: bool, right: bool) -> bool {
     left || right
 }
 
+// Returns true, if both left and right are true
 pub fn both (left: bool, right: bool) -> bool {
     left && right
 }
