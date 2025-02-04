e15900597874bf8f4bae2e93556b273bc0e6f48a
Aigars Šķiliņš <null>
2025-02-04 23:38:05 +0200

    Forced inline of functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,17 @@
 // Returns true, if not false 
+#[inline(always)]
 pub fn not (expr: bool) -> bool {
     !expr
 }
 
 // Returns true, if either left or right is true
+#[inline(always)]
 pub fn either (left: bool, right: bool) -> bool {
     left || right
 }
 
 // Returns true, if both left and right are true
+#[inline(always)]
 pub fn both (left: bool, right: bool) -> bool {
     left && right
 }
